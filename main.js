5a5ca352bb88d9b85570d7ef4358b94857fb61df
Tess Galloway <mariah.tess.galloway@gmail.com>
2020-09-16 14:31:06 -0600

    Worked on debugging the HTML, JS, and CSS files - trying to get the red square to appear

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 let container = document.querySelector('.container');
-let divTag = document.createElement('div');
+let gridSquare = document.querySelector('.grid-square');
+let divTag = document.createElement('.div');
+
+divTag.style.background = "red";
+divTag.style.height = "10px";
+divTag.style.width = "10px";
+divTag.style.margin = "2px";
+divTag.style.borderRadius = "2px";
 
 container.appendChild(divTag);
