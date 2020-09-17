c4050cb9557f894b51f6e192fbc5cefc1e86ea86
Tess Galloway <mariah.tess.galloway@gmail.com>
2020-09-16 19:56:33 -0600

    Fiddling with Theme/Colors

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let divTag = document.createElement('div');
 divTag.style.background = "red";
 divTag.style.height = "10px";
 divTag.style.width = "10px";
-divTag.style.margin = "2px";
 divTag.style.borderRadius = "2px";
 
 container.appendChild(divTag);
