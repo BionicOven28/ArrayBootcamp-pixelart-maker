6511df327797084a219312e1cb6255117cac158b
Tess Galloway <mariah.tess.galloway@gmail.com>
2020-09-17 22:48:44 -0600

    Updated HTML and CSS files to reflect adding a UI container, fiddled with the JS file but still stumped

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let container = document.querySelector('.container');
-let gridSquare = document.createElement('grid-square');
 let divTag = document.createElement('div');
+let container = document.querySelector('.container');
 
 divTag.style.background = "red";
 divTag.style.height = "10px";
 divTag.style.width = "10px";
-divTag.style.borderRadius = "2px";
 
-container.appendChild(divTag);
-
-console.log('I am loaded and ready!');
+for (let i = 0; i < 100; i++) {
+ 	container.appendChild(divTag);
+ 	divTag.classList.add('gridSquare');
+    console.log(`I am running correctly.`)
+	}
