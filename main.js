4d76ae83548974d9bf717e1b97a38f5a4e626692
Tess Galloway <mariah.tess.galloway@gmail.com>
2020-09-17 23:02:42 -0600

    Added buttons with incorrect styles, fiddled more with the grid code - still unsuccessful

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ divTag.style.height = "10px";
 divTag.style.width = "10px";
 
 for (let i = 0; i < 100; i++) {
- 	container.appendChild(divTag);
- 	divTag.classList.add('gridSquare');
+ 		container.appendChild(divTag);
+ 		divTag.classList.add('gridSquare');
     console.log(`I am running correctly.`)
-	}
+} 
