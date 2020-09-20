ac8c322328a2aaef9ad9aa62a60db1cde3f8644d
Tess Galloway <mariah.tess.galloway@gmail.com>
2020-09-20 17:52:07 -0600

    Adjust some code and FINALLY got 1 row of the grid

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+// Grid stuff
 let divTag = document.createElement('div');
-let container = document.querySelector('.container');
+let container = document.querySelector('.grid-container');
 
-divTag.style.background = "red";
-divTag.style.height = "10px";
-divTag.style.width = "10px";
-
-for (let i = 0; i < 100; i++) {
- 		container.appendChild(divTag);
- 		divTag.classList.add('gridSquare');
-    console.log(`I am running correctly.`)
-} 
+for (let r = 1; r < 8; r++) { // r=rows
+		let row = document.createElement('div'); // creates the row
+    row.classList.add('row');
+ 		container.appendChild(row);
+    console.log(`The rows work correctly.`);
+    	for (let c = 0; c < 8; c++) { // c=columns
+      	let cell = document.createElement('div'); // creates the cell
+        cell.classList.add('grid-square');
+        row.appendChild(cell);
+        console.log(`The cells work correctly.`);
+      }
+}
